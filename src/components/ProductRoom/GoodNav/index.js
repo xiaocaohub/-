972563d07644cab721cc173483ad3c467093388b
elegancexy <9486370+elegancexy@user.noreply.gitee.com>
2024-08-17 19:29:12 +0800

    nav

--- src/components/ProductRoom/GoodNav/index.js
+++ src/components/ProductRoom/GoodNav/index.js
@@ -4,6 +4,81 @@ import { CaretRightOutlined , UpOutlined,  DownOutlined } from '@ant-design/icon
 
 import "./index.css";
 class GoodNav extends React.Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+
+            styleHover: true,
+            styleNavArr: [
+                {
+                    id: 0,
+                    title: "现代",
+
+                    checked: false
+                },
+                {
+                    id: 1,
+                    title: "极简",                
+
+                    checked: false
+                },
+                {
+                    id: 2,
+                    title: "轻奢",
+                    checked: false
+                },
+                {
+                    id: 3,
+                    title: "中式",
+                    checked: false
+                },
+                {
+                    id: 4,
+                    title: "新中式",
+
+
+                    checked: false
+                },
+                {
+                    id: 5,
+
+                    title: "欧美",
+                    checked: false
+                },
+                {
+                    id: 6,
+                    title: "北欧",
+                    checked: false
+                },
+                {
+                    id: 7,
+                    title: "中古风",
+                    checked: false
+                },
+                {
+                    id: 8,
+                    title: "其他",
+                    checked: false
+                }
+            ]
+        }
+    }
+    styleSelectMoreFn = ()=> {
+    
+
+        let styleHover = !this.state.styleHover;
+        this.setState({
+            styleHover: styleHover
+        })
+    }
+
+    selectStyleFn = (index)=> {
+        let styleNavArr = this.state.styleNavArr;
+        styleNavArr[index].checked = !styleNavArr[index].checked;
+        this.setState({
+            styleNavArr: styleNavArr
+        })
+    }
     render () {
 
         return (
@@ -58,25 +133,30 @@ class GoodNav extends React.Component {
                         <div className="show_btn">收起 <DownOutlined /></div> */}
                     </div>
 
-                    <div className="nav_list_con">
+                    <div className= {this.state.styleHover?"nav_list_con":"nav_list_con on"}>
                         <div className="title">风格</div>
-                        <ul className="nav_list">
-                            <li>现代</li>
-                            <li>极简</li>
-                            <li>轻奢</li>
-                            <li>中式</li>
-                            <li>新中式</li>
-                            <li>欧美</li>
-                            <li>北欧</li>
-                            <li>中古风</li>
-                            <li>其他</li>
+                     
+                     
+                     
+                        <ul className = "nav_list on">
+                            {this.state.styleNavArr.map((item, index)=>{
+                                return ( this.state.styleHover? (<li>{item.title}</li>) : 
+                                            
+                                            (<li onClick={()=>{this.selectStyleFn(index)}}>
+                                                <div className = {item.checked?"select on": "select"}></div>
+                                                <span>{item.title}</span>
+                                            </li>)
+                                )
+                            })}
+                            
                         </ul>
 
-                        <div className="slect_more">+多选</div>
+
+                        <div className="slect_more" onClick={this.styleSelectMoreFn}>+多选</div>
                         <div className="show_btn">收起 <DownOutlined /></div>
                     </div>
 
-                    <div className="nav_list_con">
+                    {/* <div className="nav_list_con">
                         <div className="title">系列</div>
                         <ul className="nav_list">                        
                             <li> 全屋·柏林M1</li>
@@ -89,9 +169,9 @@ class GoodNav extends React.Component {
                             <li>全屋·云想Y1</li>
                         </ul>
 
-                        {/* <div className="slect_more">+多选</div> */}
+                        <div className="slect_more">+多选</div>
                         <div className="show_btn">收起 <DownOutlined /></div>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         )
